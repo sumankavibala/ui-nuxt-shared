b234ee119db50e727c0b4d259d828a2840590da7
sumankavibala <sumankavibala@gmail.com>
2025-06-03 22:23:59 +0530

    Initial commit with alert component

---
+++ components/Alert.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- Button that triggers the alert -->
+    <button @click="showAlert">Show Alert</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlertComponent',
+  props: {
+    content: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    showAlert() {
+      alert(this.content); // Show content in an alert box
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  padding: 10px;
+  background-color: #007BFF;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+</style>
